2da05ac98438ce5eea4e132d25558824e4fbbbcc
Sven Jansen <sven.jansen@devicetrust.com>
2023-03-13 14:19:22 +0100

    .

---
+++ GitHub/update-all.ps1
@@ -0,0 +1,8 @@
+Get-ChildItem C:\SharingTools\GIT\deviceTRUST-iac-resources | Where-Object{$_.Name -like "*adc*"} | ForEach-Object{
+
+    cd $_.FullName
+    git add *
+    git commit --all -m "."
+    git push
+
+}
