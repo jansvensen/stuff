b715a2ddb37ea6eaa0c88ecb7d15fe072c4cfb45
Sven Jansen <sven.jansen@devicetrust.com>
2023-03-16 09:55:59 +0100

    .

--- GitHub/update-all.ps1
+++ GitHub/update-all.ps1
@@ -1,8 +1,8 @@
-Get-ChildItem C:\SharingTools\GIT\deviceTRUST-iac-resources | Where-Object{$_.Name -like "*adc*"} | ForEach-Object{
+Get-ChildItem C:\SharingTools\GIT\jansvensen | Where-Object{$_.Name -like "*terraform*"} | ForEach-Object{
 
     cd $_.FullName
     git add *
     git commit --all -m "."
-    # git push
+    git push
 
 }
