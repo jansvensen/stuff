7641f6c2cdfc19fc652b39efe633a9450ffb973c
Sven Jansen <sven.jansen@devicetrust.com>
2023-08-30 14:33:45 +0200

    .

--- GitHub/update-all.ps1
+++ GitHub/update-all.ps1
@@ -1,7 +1,7 @@
 Get-ChildItem C:\SharingTools\GIT\jansvensen | Where-Object{$_.Name -like "*ansible*"} | ForEach-Object{
 
     cd $_.FullName
-    git add *
+    # git add *
     git commit --all -m "."
     # git push
 
