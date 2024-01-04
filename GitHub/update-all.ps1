11941cda08d911efe0fb2b4db6437b85d40938dd
Sven Jansen <github@jansvensen.de>
2024-01-04 19:39:35 +0100

    .

--- GitHub/update-all.ps1
+++ GitHub/update-all.ps1
@@ -1,4 +1,4 @@
-Get-ChildItem C:\SharingTools\GIT\jansvensen | Where-Object{$_.Name -like "*ansible*"} | ForEach-Object{
+Get-ChildItem C:\Sync-Tools\GIT\jansvensen | Where-Object{$_.Name -like "*ansible*"} | ForEach-Object{
 
     cd $_.FullName
     # git add *
