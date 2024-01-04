82fb7afcb80d655ce0736c6672168f392fc8edc3
Sven Jansen <github@jansvensen.de>
2024-01-04 19:38:45 +0100

    .

--- PowerShell/Import-Export-GPO.ps1
+++ PowerShell/Import-Export-GPO.ps1
@@ -1,23 +1,29 @@
 <# Export #>
+
+# Set the source path for exporting GPOs
 $SourcePath = "c:\_GPO"
 mkdir $SourcePath
 
-Get-GPO -all | ForEach-Object{
+# Loop through all GPOs and export each one to a separate directory
+Get-GPO -all | ForEach-Object {
     $GPOName = $_.DisplayName
     $GPOID = $_.ID
-    $BackupPath = $SourcePath+ "\" + $GPOName
+    $BackupPath = $SourcePath + "\" + $GPOName
     mkdir $BackupPath
     Backup-GPO -Path $BackupPath -Guid $GPOID #-WhatIf
-} 
+}
 
-break
+
 <# Import #>
+
+# Set the source path for importing GPOs
 $SourcePath = "c:\_GPO"
 
-Get-Childitem $BackupPath | ForEach-Object{
-
+# Loop through all directories in the source path (each containing a GPO backup)
+Get-Childitem $BackupPath | ForEach-Object {
+    # Retrieve the GUID of the GPO backup
     $BackupGUID = (Get-ChildItem $_.FullName).Name
 
+    # Import the GPO from the backup
     Import-GPO -BackupId $BackupGUID -Path $_.FullName -TargetName $_.Name -CreateIfNeeded #-WhatIf
-
 }
