364f0a727e4c2cfeae0da68567d4c57ceb72b0f1
Sven Jansen <github@jansvensen.de>
2025-02-26 13:06:49 +0100

    .

--- homelab/domain/install-dc.ps1
+++ homelab/domain/install-dc.ps1
@@ -1,4 +1,4 @@
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
 
 $password = Read-Host "Enter the SafeModeAdminPassword" -AsSecureString
-Install-ADDSForest -DomainName sjansen.local -DomainNetBiosName SJANSEN -DomainMode WinThreshold -ForestMode WinThreshold -SkipPreChecks -InstallDns:$true -SafeModeAdministratorPassword $password -Force
+Install-ADDSForest -DomainName jansvensen.de -DomainNetBiosName JANSVENSEN -DomainMode WinThreshold -ForestMode WinThreshold -SkipPreChecks -InstallDns:$true -SafeModeAdministratorPassword $password -Force
