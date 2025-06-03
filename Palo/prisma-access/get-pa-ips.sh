84e5103c9fdd8b6a427615eb6adeaaa3314970b2
Sven Jansen <github@jansvensen.de>
2025-06-03 09:10:45 +0200

    .

---
+++ Palo/prisma-access/get-pa-ips.sh
@@ -0,0 +1,5 @@
+curl \
+ -X POST \
+ --data @get-pa-ips.txt \
+ -H header-api-key:myhnkjxak5__rKrpkAwKKA1iYi8yrfNbB8f6B7VMEaLW9LM0H8Mn \
+ -u "https://api.prod.datapath.prismaaccess.com/getPrismaAccessIP/v2"
